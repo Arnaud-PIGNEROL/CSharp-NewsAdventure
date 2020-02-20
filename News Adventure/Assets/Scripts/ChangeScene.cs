8eed58f178f23ce468c55ba75d211ef82981e0ba
Aude Heisserer <aude.heisserer@efrei.net>
2020-02-20 17:34:40 +0100

    Première map accessible depuis la première news

---
+++ News Adventure/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string MapToLoad;
+
+   //Function called when page pressed
+    public void change()
+    {
+        SceneManager.LoadScene(MapToLoad);
+    }
+}
