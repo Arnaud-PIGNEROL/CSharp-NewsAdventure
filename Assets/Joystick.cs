b805d01421b8aa0f298c40c60208e717d2f6cc85
Tr4hern <arnaud.pignerol@gmail.com>
2020-02-24 15:10:05 +0100

    Boutons pour attaque + délai entre attaques

--- Assets/Joystick.cs
+++ Assets/Joystick.cs
@@ -7,12 +7,24 @@ public class Joystick : MonoBehaviour
     public Transform player;
     public float speed = 5.0f;
     public float touchArea = 2.6f;
+    public float button = 1.0f;
     private bool touchStart = false;
     private Vector2 pointA;
     private Vector2 pointB;
+
+    private Vector2 pointCAC;
+    private Vector2 pointMid;
+    private Vector2 pointRange;
         
     public RectTransform circle;
     public RectTransform outerCircle;
+
+    public RectTransform attackCAC;
+    public RectTransform attackMid;
+    public RectTransform attackRange;
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
 
     void Start()
     {
@@ -24,6 +36,19 @@ public class Joystick : MonoBehaviour
 
         circle.GetComponent<SpriteRenderer>().enabled = true;
         outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+
+
+        pointCAC = new Vector2(attackCAC.transform.position.x, attackCAC.transform.position.y);
+        pointMid = new Vector2(attackMid.transform.position.x, attackMid.transform.position.y);
+        pointRange = new Vector2(attackRange.transform.position.x, attackRange.transform.position.y);
+
+        attackCAC.transform.position = pointCAC;
+        attackMid.transform.position = pointMid;
+        attackRange.transform.position = pointRange;
+
+        attackCAC.GetComponent<SpriteRenderer>().enabled = true;
+        attackMid.GetComponent<SpriteRenderer>().enabled = true;
+        attackRange.GetComponent<SpriteRenderer>().enabled = true;
     }
 
     void Update()
@@ -32,7 +57,46 @@ public class Joystick : MonoBehaviour
         {
             pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
 
-            if ((pointB - pointA).sqrMagnitude <= touchArea)     //float Vector2.sqrMagnitude
+            if ((pointB - pointCAC).sqrMagnitude <= button)             // cac attack
+            {
+                if (timeBtwAttack <= 0)
+                {
+                    Debug.Log("cac");
+                    timeBtwAttack = startTimeBtwAttack;
+                }
+                else
+                {
+                    Debug.Log("time : " + timeBtwAttack);
+                    timeBtwAttack -= Time.deltaTime;
+                }
+            }
+            else if ((pointB - pointMid).sqrMagnitude <= button)        // mid range attack
+            {
+                if (timeBtwAttack <= 0)
+                {
+                    Debug.Log("mid");
+                    timeBtwAttack = startTimeBtwAttack;
+                }
+                else
+                {
+                    Debug.Log("time : " + timeBtwAttack);
+                    timeBtwAttack -= Time.deltaTime;
+                }
+            }
+            else if ((pointB - pointRange).sqrMagnitude <= button)      // long range attack
+            {
+                if (timeBtwAttack <= 0)
+                {
+                    Debug.Log("range");
+                    timeBtwAttack = startTimeBtwAttack;
+                }
+                else
+                {
+                    Debug.Log("time : " + timeBtwAttack);
+                    timeBtwAttack -= Time.deltaTime;
+                }
+            }
+            else if ((pointB - pointA).sqrMagnitude <= touchArea)     //float Vector2.sqrMagnitude
                 touchStart = true;
             else
             {
