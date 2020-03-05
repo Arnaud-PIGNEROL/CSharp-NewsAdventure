9d696179e7d636153042e03a35a71b0aacc959b2
Aude Heisserer <aude.heisserer@efrei.net>
2020-03-05 20:51:48 +0100

    Connexion des branches Player et joystic avec erreur
    Le joystick ne reste pas en place

---
+++ News Adventure/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+
+    void LateUpdate()
+    {
+        //Debug.Log(target.position);
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //Camera.main.transform.position = smoothedPosition;
+        transform.position = new Vector3(target.position.x, 0, transform.position.z);
+        //Debug.Log("Moving");
+    }
+}
--- News Adventure/Assets/Scripts/Joystick.cs
+++ News Adventure/Assets/Scripts/Joystick.cs
@@ -4,58 +4,134 @@ using UnityEngine;
 
 public class Joystick : MonoBehaviour
 {
-    public Animator animator;
     public Transform player;
     public float speed = 5.0f;
+    public float touchArea = 2.6f;
+    public float button = 1.0f;
     private bool touchStart = false;
     private Vector2 pointA;
     private Vector2 pointB;
+
+    private Vector2 pointCAC;
+    private Vector2 pointMid;
+    private Vector2 pointRange;
         
-    public Transform circle;
-    public Transform outerCircle;
+    public RectTransform circle;
+    public RectTransform outerCircle;
+
+    public RectTransform attackCAC;
+    public RectTransform attackMid;
+    public RectTransform attackRange;
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
 
     void Start()
     {
+        //Crée un vecteur entre la position du cercle interne et du cercle externe
         pointA = new Vector2(circle.transform.position.x, circle.transform.position.y);
+        Debug.Log("Position of a" + pointA.x + " " + pointA.y);
         pointB = pointA;
-        //pointA = Camera.main.ScreenToWorldPoint(new Vector3(circle.transform.position.x, circle.transform.position.y, Camera.main.transform.position.z));
+        
         circle.transform.position = pointA;
         outerCircle.transform.position = pointA;
+
         circle.GetComponent<SpriteRenderer>().enabled = true;
         outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        Debug.Log("Out of position of a");
+
+        /*pointCAC = new Vector2(attackCAC.transform.position.x, attackCAC.transform.position.y);
+        pointMid = new Vector2(attackMid.transform.position.x, attackMid.transform.position.y);
+        pointRange = new Vector2(attackRange.transform.position.x, attackRange.transform.position.y);
+
+        attackCAC.transform.position = pointCAC;
+        attackMid.transform.position = pointMid;
+        attackRange.transform.position = pointRange;
+
+        attackCAC.GetComponent<SpriteRenderer>().enabled = true;
+        attackMid.GetComponent<SpriteRenderer>().enabled = true;
+        attackRange.GetComponent<SpriteRenderer>().enabled = true;*/
     }
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            touchStart = true;
+            Debug.Log("Here !");
             pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            Debug.Log("The point B is : " + pointB);
+            if ((pointB - pointCAC).sqrMagnitude <= button)             // cac attack
+            {
+                if (timeBtwAttack <= 0)
+                {
+                    Debug.Log("cac");
+                    timeBtwAttack = startTimeBtwAttack;
+                }
+                else
+                {
+                    Debug.Log("time : " + timeBtwAttack);
+                    timeBtwAttack -= Time.deltaTime;
+                }
+            }
+            else if ((pointB - pointMid).sqrMagnitude <= button)        // mid range attack
+            {
+                if (timeBtwAttack <= 0)
+                {
+                    Debug.Log("mid");
+                    timeBtwAttack = startTimeBtwAttack;
+                }
+                else
+                {
+                    Debug.Log("time : " + timeBtwAttack);
+                    timeBtwAttack -= Time.deltaTime;
+                }
+            }
+            else if ((pointB - pointRange).sqrMagnitude <= button)      // long range attack
+            {
+                if (timeBtwAttack <= 0)
+                {
+                    Debug.Log("range");
+                    timeBtwAttack = startTimeBtwAttack;
+                }
+                else
+                {
+                    Debug.Log("time : " + timeBtwAttack);
+                    timeBtwAttack -= Time.deltaTime;
+                }
+            }
+            else if ((pointB - pointA).sqrMagnitude <= touchArea)     //float Vector2.sqrMagnitude
+            {
+                touchStart = true;
+            }
+            else
+            {
+                pointB = pointA;
+                touchStart = false;
+            }
         }
         else
         {
+            Debug.Log("Hellow");
             pointB = pointA;
             touchStart = false;
         }
     }
 
     private void FixedUpdate()
-    {
+    { 
         if (touchStart)
         {
-            Vector2 direction = Vector2.ClampMagnitude(pointB - pointA, 1.0f);
+            Debug.Log("Wtf");
+            Vector2 direction = Vector2.ClampMagnitude(pointB - pointA, 1.0f);  // empêche le boutton de quitter le cercle
             moveCharacter(direction);
-            animator.SetFloat("Horizontal", direction.x);
-            animator.SetFloat("Vertical", direction.y);
-            animator.SetFloat("Magnitude", direction.magnitude);
-            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
+            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.x);
+            pointA = new Vector2(pointA.x + player.position.x, pointA.y + player.position.y);
         }
         else
         {
+            Debug.Log("inside this");
             circle.transform.position = new Vector2(pointA.x, pointA.y);
-            animator.SetFloat("Horizontal", 0);
-            animator.SetFloat("Vertical", 0);
-            animator.SetFloat("Magnitude", 0);
+            Debug.Log("position " + pointA.x + " " + pointA.y);
         }
     }
 
