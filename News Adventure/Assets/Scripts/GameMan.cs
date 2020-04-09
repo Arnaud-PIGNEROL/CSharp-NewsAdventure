6f6c1c655dc89e4f548eff11858a769fa709bdb9
Trahern <spammenotnameho@gmail.com>
2020-04-09 21:01:09 +0200

    Ecran de fin de jeu + ajout des quelques sons in-game

---
+++ News Adventure/Assets/Scripts/GameMan.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMan : MonoBehaviour
+{
+
+    private bool end;
+    public GameObject victory;
+    public GameObject defeat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        victory.SetActive(false);
+        defeat.SetActive(false);
+        end = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.V) && end == false)
+        {
+            Win();
+            end = true;
+        }
+         
+        if (Input.GetKey(KeyCode.L) && end == false)
+        {
+            Lose();
+            end = true;
+        }
+    }
+
+
+    private void Win()
+    {
+        FindObjectOfType<AudioManager>().Play("Victory");
+        victory.SetActive(true);
+        FindObjectOfType<AudioManager>().Stop("Game");
+    }
+    
+    public void Lose()
+    {
+        FindObjectOfType<AudioManager>().Play("Loss");
+        defeat.SetActive(true);
+        FindObjectOfType<AudioManager>().Stop("Game");
+    }
+
+}
