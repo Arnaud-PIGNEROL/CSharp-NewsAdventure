5811879eca24a2f258c09f9f36af2e046fdf40cc
Trahern <spammenotnameho@gmail.com>
2020-05-09 14:15:09 +0200

    correction du problème du merge

--- News Adventure/Assets/Scripts/ButtonAttack.cs
+++ News Adventure/Assets/Scripts/ButtonAttack.cs
@@ -16,7 +16,7 @@ public class ButtonAttack : MonoBehaviour
 
     public void Start()
     {
-       
+
     }
     public void CAC() // button attack cac
     {
@@ -32,7 +32,7 @@ public class ButtonAttack : MonoBehaviour
             timeBtwAttack -= Time.deltaTime;
         }
     }
-    
+
     public void Mid() // button attack mid range
     {
         if (timeBtwAttack <= 0)
@@ -47,10 +47,9 @@ public class ButtonAttack : MonoBehaviour
             timeBtwAttack -= Time.deltaTime;
         }
     }
-    
+
     public void Range() // button attack range
     {
-<<<<<<< HEAD
         timeBtwAttack -= (Time.time - timeBtwAttack);
         //Debug.Log("BTW : " + timeBtwAttack);
         if (timeBtwAttack < Time.time)
@@ -82,35 +81,6 @@ public class ButtonAttack : MonoBehaviour
             }
             PLAYER.GetComponent<Player>().setDrop(true);
             timeBtwAttack = Time.time + startTimeBtwAttack;
-=======
-        timeBtwAttack -= (Time.time - timeBtwAttack); // to set a minimal time bewteen 2 attacks
-        Debug.Log("BTW : " + timeBtwAttack);
-        if (timeBtwAttack < Time.time) // if the time between the old attack and the new attack isn't too short
-        {
-            Transform PLAYER = GameObject.FindGameObjectWithTag("Player").transform;
-
-
-            //if the joystick is upwards and if the joystick is more trought the vertical axis than trought the horizontal axis 
-            // because the joystick can be on y=0.1 & x=0.9 so the projectile has to be to the right, even if y>0 (we consider y axis as vertical)
-            if (joy.Vertical > 0 && (Mathf.Abs(joy.Vertical) > Mathf.Abs(joy.Horizontal))) // projectile up
-            {
-                Instantiate(projectileUp, ShotPoint.position, transform.rotation);
-            }
-            else if (joy.Vertical < 0 && (Mathf.Abs(joy.Vertical) > Mathf.Abs(joy.Horizontal))) // projectile down
-            {
-                Instantiate(projectileDown, ShotPoint.position, transform.rotation);
-            }
-            else if (joy.Horizontal < 0 && (Mathf.Abs(joy.Horizontal) > Mathf.Abs(joy.Vertical))) // projectile right
-            {
-                Instantiate(projectileRight, ShotPoint.position, transform.rotation);
-            }
-            else
-            {
-                Instantiate(projectileLeft, ShotPoint.position, transform.rotation);  // projectile left
-            }
-            PLAYER.GetComponent<Player>().setDrop(true); // if we attack, the animal is droped
-            timeBtwAttack = Time.time + startTimeBtwAttack; // init a timer to wait before making a new attack
->>>>>>> 8f834dc1e7eb8a4b7fc59468b123408f1cc6a3b4
         }
     }
 }
